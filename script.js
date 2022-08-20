1f394ab0d5b3f89cfab21fc2b55aae1b3ab947c6
rooklift <16438795+rooklift@users.noreply.github.com>
2022-08-20 14:16:21 +0100

    use Array.from()

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://online-go.com/*
 // @grant       none
-// @version     1.0
+// @version     1.0.1
 // @author      Rooklift
 // @description Adjusts the OGS audio timer by 2 seconds
 // ==/UserScript==
@@ -25,23 +25,23 @@ if (!window.ogs_timer_adjuster_monkey) {
 
 				let o = window.sprite_packs[key].definitions;
 
-				if (o[1] !== o["silence"]) {
+				if (o[1][0] !== o["silence"][0] || o[1][1] !== o["silence"][1]) {
 
 					console.log(`Adjusting window.sprite_packs["${key}"].definitions`);
 					
 					// Suppress all warnings for 13 seconds and higher
 					for (let n = 60; n >= 13; n--) {
-						o[n] = o["silence"];
+						o[n] = Array.from(o["silence"]);
 					}
 					
 					// Adjust warning sounds between 12 and 3 seconds to use the sounds for 10 to 1
 					for (let n = 12; n >= 3; n--) {
-						o[n] = o[n - 2];
+						o[n] = Array.from(o[n - 2]);
 					}
 					
 					// Suppress warnings for 2 and 1 seconds
 					for (let n = 2; n >= 1; n--) {
-						o[n] = o["silence"];
+						o[n] = Array.from(o["silence"]);
 					}
 
 				}
